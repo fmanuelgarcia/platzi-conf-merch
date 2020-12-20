83ab5d20a9e5976aa7c9aa73a0b4b7d44028156c
Franklin Garcia <fmanuelgarcia@icloud.com>
2020-12-19 19:50:23 -0500

    final del curso

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+import Map from '../components/Map';
+import useGoogleAddress from '../Hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
+  const { state } = useContext(AppContext);
+  const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address);
+
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>Ender, Gracias por tu compra</h2>
+        <h2>{`${buyer.name}, Gracias por tu compra`}</h2>
         <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
         <div className="Success-map">
-          Google Maps
+          <Map data={location} />
         </div>
       </div>
     </div>
